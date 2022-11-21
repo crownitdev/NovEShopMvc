41ab2e4354b2d6e30c1b574540bbc410cba26b81
crownitdev <crown.itdev@gmail.com>
2022-11-21 22:10:39 +0700

    adjust methods of manage product images

--- NovEShop.Handler/Products/Dtos/ProductImageViewModel.cs
+++ NovEShop.Handler/Products/Dtos/ProductImageViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace NovEShop.Handler.Products.Dtos
+﻿using System;
+
+namespace NovEShop.Handler.Products.Dtos
 {
     public class ProductImageViewModel
     {
         public int Id { get; set; }
-        public string FilePath { get; set; }
+        public string Caption { get; set; }
+        public string ImagePath { get; set; }
         public bool IsDefault { get; set; }
         public long FileSize { get; set; }
+        public int SortOrder { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
