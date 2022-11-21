eeb3f82e3d830ad0e05d730a13c48f6c9bb63925
crownitdev <crown.itdev@gmail.com>
2022-11-21 22:29:52 +0700

    add implementation for GetListProductImageQuery

--- NovEShop.Handler/Products/Dtos/ProductImageViewModel.cs
+++ NovEShop.Handler/Products/Dtos/ProductImageViewModel.cs
@@ -5,6 +5,7 @@ namespace NovEShop.Handler.Products.Dtos
     public class ProductImageViewModel
     {
         public int Id { get; set; }
+        public int ProductId { get; set; }
         public string Caption { get; set; }
         public string ImagePath { get; set; }
         public bool IsDefault { get; set; }
