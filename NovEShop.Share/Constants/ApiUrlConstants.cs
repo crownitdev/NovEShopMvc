163a0fc0d614807ec4de9a99a2c72350c5226d5f
crownitdev <crown.itdev@gmail.com>
2022-12-02 02:17:21 +0700

    config api url

---
+++ NovEShop.Share/Constants/ApiUrlConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace NovEShop.Share.Constants
+{
+    public static class ApiUrlConstants
+    {
+        public const string AdminAppUrl = "https://localhost:5002";
+        public const string ServeApiUrl = "https://localhost:5001";
+    }
+}
